7fa73b42e5b333acb6333d8471839f7e23c105de
Nathan Seixeiro <nathan.seixeiro@gmail.com>
2023-11-09 02:40:05 -0300

    index ok!

--- api/src/index.js
+++ api/src/index.js
@@ -2,26 +2,34 @@ import express from "express";
 import axios from "axios";
 import cheerio from "cheerio";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', async (req, res) => {
-    const {keyword} = req.query
-    const url = `https://www.lojasrenner.com.br/b?Ntt=${keyword}`
-// https://www.casasbahia.com.br/
-    const {data} = await axios.get(url)
-    const $ = cheerio.load(data)
+app.get('/api/scrape', async (req, res) => {
+  const { keyword } = req.query;
+  const url = `https://www.amazon.com/s?k=${keyword}`;
+
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
 
     const products = [];
 
     $('.s-result-item').each((index, element) => {
-        const title = $(element).find('.ProductBox_title__x9UGh').text();
-        products.push({ title });
-    });
-    
-    console.log(products)
-})
+      const title = $(element).find('span.a-text-normal').text();
+      const rating = $(element).find('span.a-icon-star').attr('aria-label');
+      const numReviews = $(element).find('span.a-size-base').text();
+      const imgURL = $(element).find('img.s-image').attr('src');
 
-app.listen(port, () => console.log(`✔ server is running on port: ${port}`))
+      products.push({ title, rating, numReviews, imgURL });
+    });
+
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to scrape data.' });
+  }
+});
+
+app.listen(port, () => console.log(`✔ server is running on port: ${port}`));
