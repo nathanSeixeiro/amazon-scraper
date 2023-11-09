c89433875be0a27d46895c95ab961d8b599a9c65
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-11-09 13:57:06 +0000

    comments: add comments about the logic and the process

--- api/src/index.js
+++ api/src/index.js
@@ -3,10 +3,12 @@ import axios from "axios";
 import cheerio from "cheerio";
 import cors from "cors";
 
+// basic express server
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+//use cor for allowed the frontend make a requests
 app.use(cors());
 
 app.get("/api/scrape", async (req, res) => {
@@ -14,22 +16,28 @@ app.get("/api/scrape", async (req, res) => {
     const url = `https://www.amazon.com/s?k=${keyword}`;
 
     try {
+        // get data from the url with axios
         const { data } = await axios.get(url);
+        // load data on cheerio
         const $ = cheerio.load(data);
 
         const products = [];
 
+        // get the elements what i want based on yours css selectors  
         $(".s-result-item").each((index, element) => {
             const title = $(element).find("span.a-text-normal").text();
-            const rating = $(element).find("span.a-icon-star").attr("aria-label");
+            // const rating = $(element).find("span.a-icon-star").attr("aria-label");
+            const rating = $(element).find("span.a-icon-alt").text();
             const numReviews = $(element).find("span.a-size-base").text();
             const imgURL = $(element).find("img.s-image").attr("src");
 
+            // create an object with scraped data and add it to the 'products' array
             products.push({ title, rating, numReviews, imgURL });
         });
 
         res.json(products);
     } catch (error) {
+        // error message
         res.status(500).json({ error: "Failed to scrape data." });
     }
 });
--- front/script.js
+++ front/script.js
@@ -1,107 +1,40 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const keywordInput = document.getElementById("keyword");
-//   const scrapeBtn = document.getElementById("scrapeBtn");
-//   const resultsDiv = document.getElementById("results");
-//   const delayBetweenRequests = 5000;
-
-//   scrapeBtn.addEventListener("click", async () => {
-//     const keyword = keywordInput.value;
-
-//     if (!keyword) {
-//       alert("Please enter a keyword");
-//       return;
-//     }
-
-//     try {
-//       const response = await fetch(
-//         `http://localhost:3000/api/scrape?keyword=${keyword}`
-//       );
-//       if (!response.ok) {
-//         throw new Error(`Failed to fetch data. Status: ${response.status}`);
-//       }
-//       const data = await response.json();
-//       console.log(data);
-
-//       if (Array.isArray(data)) {
-//         displayResults(data);
-//       } else {
-//         resultsDiv.innerHTML = "No products found.";
-//       }
-//     } catch (error) {
-//       console.error("Failed to fetch data:", error);
-//       resultsDiv.innerHTML = "Failed to fetch data.";
-//     }
-//   });
-
-//   function displayResults(data) {
-//     resultsDiv.innerHTML = "";
-
-//     if (data.length === 0 || !data) {
-//       resultsDiv.innerHTML = "No products found.";
-//       return;
-//     }
-
-//     const products = excludeEmptyItems(data);
-//     products.forEach((product) => {
-//       const productDiv = document.createElement("div");
-//       productDiv.className = "product";
-
-//       const title = document.createElement("h2");
-//       title.textContent = product.title;
-
-//       const rating = document.createElement("p");
-//       rating.textContent = `Rating: ${product.rating || "N/A"}`;
-
-//       const numReviews = document.createElement("p");
-//       numReviews.textContent = `Reviews: ${product.numReviews || "N/A"}`;
-
-//       const img = document.createElement("img");
-//       img.src = product.imgURL;
-//       img.alt = product.title;
-
-//       productDiv.appendChild(img);
-//       productDiv.appendChild(title);
-//       productDiv.appendChild(rating);
-//       productDiv.appendChild(numReviews);
-
-//       resultsDiv.appendChild(productDiv);
-//     });
-
-//     function excludeEmptyItems(products) {
-//       products = products.filter((product) => product.title !== "");
-//       return;
-//     }
-
-//     setTimeout(() => {
-//       isRequestPending = false;
-//     }, delayBetweenRequests);
-//   }
-// });
-
+// Wait for the DOM to be fully loaded before executing the code
 document.addEventListener("DOMContentLoaded", function () {
+  // Get references to HTML elements
   const keywordInput = document.getElementById("keyword");
   const scrapeBtn = document.getElementById("scrapeBtn");
   const resultsDiv = document.getElementById("results");
+  const delayBetweenRequests = 5000; // Delay between requests in milliseconds
 
+  // Add a click event listener to the "Scrape" button
   scrapeBtn.addEventListener("click", async () => {
+    // Get the search keyword from the input field
     const keyword = keywordInput.value;
 
+    // Check if the keyword input is empty and show an alert if it is
     if (!keyword) {
       alert("Please enter a keyword");
       return;
     }
 
     try {
+      // Send a fetch request to the API with the entered keyword
       const response = await fetch(
-        `http://localhost:3000/api/scrape?keyword=${keyword}`
+        `https://fantastic-capybara-gj575xjx4r4hpv7j-3000.app.github.dev/api/scrape?keyword=${keyword}`
       );
+
+      // Check if the response status is OK, or else throw an error
       if (!response.ok) {
         throw new Error(`Failed to fetch data. Status: ${response.status}`);
       }
+
+      // Parse the JSON response
       const data = await response.json();
       console.log(data);
 
+      // Check if the data is an array and contains at least one item
       if (Array.isArray(data) && data.length > 0) {
+        // Display the results on the page
         displayResults(data);
       } else {
         resultsDiv.innerHTML = "No products found.";
@@ -112,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  // Function to display the results on the page
   function displayResults(data) {
     resultsDiv.innerHTML = "";
 
@@ -146,4 +80,9 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
+
+  // Set a timeout to prevent rapid consecutive requests
+  setTimeout(() => {
+    isRequestPending = false;
+  }, delayBetweenRequests);
 });
