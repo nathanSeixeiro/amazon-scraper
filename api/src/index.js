5c615e1af2dcc4e44476a68f953a167a4958256e
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-11-09 13:15:41 +0000

    style: fix code style

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import axios from "axios";
 import cheerio from "cheerio";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 const port = 3000;
@@ -9,29 +9,29 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/scrape', async (req, res) => {
-  const { keyword } = req.query;
-  const url = `https://www.amazon.com/s?k=${keyword}`;
+app.get("/api/scrape", async (req, res) => {
+    const { keyword } = req.query;
+    const url = `https://www.amazon.com/s?k=${keyword}`;
 
-  try {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
+    try {
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
 
-    const products = [];
+        const products = [];
 
-    $('.s-result-item').each((index, element) => {
-      const title = $(element).find('span.a-text-normal').text();
-      const rating = $(element).find('span.a-icon-star').attr('aria-label');
-      const numReviews = $(element).find('span.a-size-base').text();
-      const imgURL = $(element).find('img.s-image').attr('src');
+        $(".s-result-item").each((index, element) => {
+            const title = $(element).find("span.a-text-normal").text();
+            const rating = $(element).find("span.a-icon-star").attr("aria-label");
+            const numReviews = $(element).find("span.a-size-base").text();
+            const imgURL = $(element).find("img.s-image").attr("src");
 
-      products.push({ title, rating, numReviews, imgURL });
-    });
+            products.push({ title, rating, numReviews, imgURL });
+        });
 
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to scrape data.' });
-  }
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to scrape data." });
+    }
 });
 
 app.listen(port, () => console.log(`✔ server is running on port: ${port}`));
